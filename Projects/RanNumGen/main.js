c19c2b234af672fffaa5a985d141306ed264344f
arjack0815@gmail.com <alissa.j636@nebo.local>
2023-03-18 22:55:59 -0600

    Confetti when you win.

--- Projects/RanNumGen/main.js
+++ Projects/RanNumGen/main.js
@@ -1,5 +1,5 @@
 
-let randomNumber = Math.floor(Math.random() * 100) +1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
@@ -11,68 +11,174 @@ guessField.focus();
 
 
 
- function checkGuess(){
+function checkGuess() {
     const userGuess = Number(guessField.value);
-    if (guessCount === 1){
-     guesses.textContent = 'Previous guesses: ';
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
     }
-    guesses.textContent += userGuess + ' ' ;
+    guesses.textContent += userGuess + ' ';
 
-    if(userGuess === randomNumber){
-     lastResult.textContent = 'Congrats! Its right.';
-     lastResult.style.backgroundColor = 'green';
-     lowOrHi.textContent ='';
-         setGameOver();
-     }
-     else if (guessCount === 10) {
-lastResult.textContent = '!!!GAME OVER!!!';
-lowOrHi.textContent = '';
-setGameOver();
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congrats! Its right.';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        let W = window.innerWidth;
+        let H = window.innerHeight;
+        const canvas = document.getElementById("canvas");
+        const context = canvas.getContext("2d");
+        const maxConfettis = 100;
+        const particles = [];
+
+        const possibleColors = [
+            "DodgerBlue",
+            "OliveDrab",
+            "Gold",
+            "Pink",
+            "SlateBlue",
+            "LightBlue",
+            "Gold",
+            "Violet",
+            "PaleGreen",
+            "SteelBlue",
+            "SandyBrown",
+            "Chocolate",
+            "Crimson"
+        ];
+
+        function randomFromTo(from, to) {
+            return Math.floor(Math.random() * (to - from + 1) + from);
+        }
+
+        function confettiParticle() {
+            this.x = Math.random() * W; // x
+            this.y = Math.random() * H - H; // y
+            this.r = randomFromTo(11, 33); // radius
+            this.d = Math.random() * maxConfettis + 11;
+            this.color =
+                possibleColors[Math.floor(Math.random() * possibleColors.length)];
+            this.tilt = Math.floor(Math.random() * 33) - 11;
+            this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+            this.tiltAngle = 0;
+
+            this.draw = function () {
+                context.beginPath();
+                context.lineWidth = this.r / 2;
+                context.strokeStyle = this.color;
+                context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+                context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+                return context.stroke();
+            };
+        }
+
+        function Draw() {
+            const results = [];
+
+            // Magical recursive functional love
+            requestAnimationFrame(Draw);
+
+            context.clearRect(0, 0, W, window.innerHeight);
+
+            for (var i = 0; i < maxConfettis; i++) {
+                results.push(particles[i].draw());
+            }
+
+            let particle = {};
+            let remainingFlakes = 0;
+            for (var i = 0; i < maxConfettis; i++) {
+                particle = particles[i];
+
+                particle.tiltAngle += particle.tiltAngleIncremental;
+                particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+                particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+                if (particle.y <= H) remainingFlakes++;
+
+                // If a confetti has fluttered out of view,
+                // bring it back to above the viewport and let if re-fall.
+                if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+                    particle.x = Math.random() * W;
+                    particle.y = -30;
+                    particle.tilt = Math.floor(Math.random() * 10) - 20;
+                }
+            }
+
+            return results;
+        }
+
+        window.addEventListener(
+            "resize",
+            function () {
+                W = window.innerWidth;
+                H = window.innerHeight;
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+            },
+            false
+        );
+
+        // Push new confetti objects to `particles[]`
+        for (var i = 0; i < maxConfettis; i++) {
+            particles.push(new confettiParticle());
+        }
+
+        // Initialize
+        canvas.width = W;
+        canvas.height = H;
+        Draw();
+
+        setGameOver();
+    }
+    else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        lowOrHi.textContent = '';
+        setGameOver();
+    }
+    else {
+        lastResult.textContent = 'Sorry! Wrong!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Guess is too low!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Guess is too high!';
+
+        }
+    }
+
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+
 }
-else{
-         lastResult.textContent = 'Sorry! Wrong!';
-         lastResult.style.backgroundColor='red';
-         if (userGuess < randomNumber){
-             lowOrHi.textContent= 'Guess is too low!';
-         } else if(userGuess > randomNumber){
-             lowOrHi.textContent = 'Guess is too high!';
-         }
-     }
-     
+guessSubmit.addEventListener('click', checkGuess);
 
-     guessCount++;
-     guessField.value ='';
-     guessField.focus(); 
-    
- }
- guessSubmit.addEventListener('click', checkGuess);
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
 
-    function setGameOver(){
-        guessField.disabled = true;
-        guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start New Game';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
 
-        resetButton = document.createElement('button');
-        resetButton.textContent = 'Start New Game';
-        document.body.append(resetButton);
-        resetButton.addEventListener('click', resetGame);
+function resetGame() {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+        resetPara.textContent = '';
     }
 
-    function resetGame(){
-        guessCount = 1;
 
-        const resetParas = document.querySelectorAll('.resultParas p');
-for (const resetPara of resetParas) {
-resetPara.textContent = '';
-     }
+    resetButton.parentNode.removeChild(resetButton);
 
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    canvas.
 
-        resetButton.parentNode.removeChild(resetButton);
-
-        guessField.disabled = false;
-        guessSubmit.disabled = false;
-        guessField.value = '';
-        guessField.focus();
-
-        lastResult.style.backgroundColor='';
-        randomNumber = Math.floor(Math.random() *100)+1;
-    }
+    lastResult.style.backgroundColor = '';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
